316aad87e1d46a5867f0694d0166e04e256cf1a5
Dhiraj Kumar <selenophile.com>
2026-03-25 09:32:46 +0530

    Day 1: Add odd-even number checker program

---
+++ 01_odd_even.py
@@ -0,0 +1,10 @@
+num = int(input("Enter the number: "))
+
+if(num%2==0):
+    print("The given number is an even number ")
+else :
+    print("The given number is an odd number")
+
+
+# num = int(input("Enter the number: "))
+# print("Even" if num % 2 == 0 else "Odd")
