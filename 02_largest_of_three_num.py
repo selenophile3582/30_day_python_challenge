d399fc48e9a3397224cf599ad0d189e81cd4eeed
Dhiraj Kumar <selenophile.com>
2026-03-26 18:26:55 +0530

    Day 02 : Add largest of three number program

---
+++ 02_largest_of_three_num.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = int(input("Enter third number:"))
+
+if a == b == c:
+    print("All numbers are same")
+elif a >= b and a >= c:
+    print(f"{a} is largest")
+elif b >= a and b >= c:
+    print(f"{b} is largest")
+else:
+    print(f"{c} is largest")
